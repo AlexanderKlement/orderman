5e9b20a74712bedbcedd45e474e5b05512d73487
Alexander Klement <alexander.klement@student.uibk.ac.at>
2018-03-08 14:55:08 +0100

    changed Fontsize, Layout, Color and made everything bigger

--- MainGui.java
+++ MainGui.java
@@ -1,8 +1,10 @@
 package orderman_lidolana_v1;
 
 import java.awt.Button;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -17,7 +19,11 @@ import java.util.List;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
+import javax.swing.plaf.FontUIResource;
 
 public class MainGui extends JFrame {
 
@@ -26,6 +32,7 @@ public class MainGui extends JFrame {
 	 */
 	private static final long serialVersionUID = 2982884721030803196L;
 
+	// TODO add clock on the upper right
 	private List<FoodButton> foodButtons;
 	private List<JButton> buttons = new ArrayList<>();
 	public JTextArea currentOrderList;
@@ -35,53 +42,63 @@ public class MainGui extends JFrame {
 		// set Elements
 		this.foodButtons = foodButtons;
 
+		//this.setDefaultLookAndFeelDecorated(true);
 		// setup main windows:
-		JFrame f = new JFrame();
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Container c = f.getContentPane();
+		JFrame mainJFrame = new JFrame();
+		mainJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container mainContainer = mainJFrame.getContentPane();
 
 		GridBagLayout gbl = new GridBagLayout();
-		c.setLayout(gbl);
+		mainContainer.setLayout(gbl);
 
 		// Create new Order:
 		OrderCreator tempOrderCreator = new OrderCreator();
+
+		// Define Fonts:
+		Font orderListFont = new Font("Serif", Font.BOLD, 27);
+		Font priceTagFont = new Font("Serif", Font.BOLD, 37);
+		Font buttonFont = new Font("Serif", Font.BOLD, 27);
+		Font dialogButtonFont = new Font("Serif", Font.PLAIN, 35);
 
 		// setup food buttons
 		Panel panelButtons = new Panel(new GridLayout(3, 3));
 		for (int i = 0; i < numberOfButtons(); i++) {
 			buttons.add(new JButton(foodButtons.get(i).getName()));
+			buttons.get(i).setFont(buttonFont);
 			buttons.get(i).addActionListener(new ActionListener() {
 
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					FoodButton tempFoodButton = getFoodButtonByText(e.getActionCommand());
 					MenuItem tempMenuItem = new MenuItem(1, tempFoodButton.getName(), null, tempFoodButton.getPrice());
-					if (tempFoodButton.hasOptions()){
-						//create options array:
+					if (tempFoodButton.hasOptions()) {
+						//Enlarge buttons:
+						UIManager.put("OptionPane.buttonFont", new FontUIResource(dialogButtonFont));
+						// create options array:
 						Object[] options1 = new Object[tempFoodButton.numberOfOptions()];
-						for(int i = 0; i < tempFoodButton.numberOfOptions(); i++) 
+						for (int i = 0; i < tempFoodButton.numberOfOptions(); i++)
 							options1[i] = tempFoodButton.getOptions().get(i);
-						int n = JOptionPane.showOptionDialog(f,//parent container of JOptionPane
-							    "Bitte auswählen", 
-							    "Optionen",
-							    JOptionPane.YES_NO_CANCEL_OPTION,
-							    JOptionPane.QUESTION_MESSAGE,
-							    null,//do not use a custom Icon
-							    options1,//the titles of buttons
-							    "null");
-						
-						//options list:
+						int n = JOptionPane.showOptionDialog(mainJFrame, // parent container of JOptionPane
+								"Bitte auswählen", "Optionen", JOptionPane.YES_NO_CANCEL_OPTION,
+								JOptionPane.QUESTION_MESSAGE, null, // do not use a custom Icon
+								options1, // the titles of buttons
+								"null");
+
+						// options list:
 						ArrayList<String> selectedOption = new ArrayList<>();
 						selectedOption.add((String) options1[n]);
 						tempMenuItem.setOptions(selectedOption);
-					} 
+					}
 					tempOrderCreator.addMenuItem(tempMenuItem);
-					
-					//Update JTextPanes (Order + Price)
-					currentOrderList.setText("");
+
+					// Update JTextPanes (Order + Price)
+
+					resetEast();
+
 					tempOrderCreator.updateTextPanel(currentOrderList);
-					
+
 					tempOrderCreator.updatePrice(priceTag);
+
 				}
 			});
 			panelButtons.add(buttons.get(i));
@@ -98,29 +115,39 @@ public class MainGui extends JFrame {
 				TicketCreator ticketCreator = new TicketCreator(tempOrderCreator);
 				ticketCreator.sendOrder();
 				tempOrderCreator.reset();
+				currentOrderList.setBackground(Color.green);
 			}
 		});
-		
-		//Textfield where the current order is shwon:
-		currentOrderList = new JTextArea(20, 40);
-		//TODO for later: make the list scrollable
-		//JScrollPane scrollPane = new JScrollPane(tfDisplay); 
+
+		// Textfield where the current order is shwon:
+		currentOrderList = new JTextArea(7, 1);
+		JScrollPane currentOrderListScrollable = new JScrollPane(currentOrderList,
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		// JScrollPane scrollPane = new JScrollPane(tfDisplay);
 		currentOrderList.setEditable(false);
-		
-		//Pricetag:
-		priceTag = new JTextArea(20,40);
+		currentOrderList.setFont(orderListFont);
+
+		// Pricetag:
+		priceTag = new JTextArea(1, 1);
 		priceTag.setEditable(false);
-		
-		
-		// Set right gridlayout
-		Panel sendAndShowGrid = new Panel(new GridLayout(3, 1));
-		sendAndShowGrid.add(currentOrderList);
-		sendAndShowGrid.add(priceTag);
-		sendAndShowGrid.add(sendButton);
+		priceTag.setFont(priceTagFont);
+
+		JPanel sendAndShowGrid = new JPanel();
+		GridBagLayout eastLayout = new GridBagLayout();
+		sendAndShowGrid.setLayout(eastLayout);
+
+		addComponent(sendAndShowGrid, eastLayout, currentOrderListScrollable, 0, 0, 1, 1, 0.1, 0.7);
+		addComponent(sendAndShowGrid, eastLayout, priceTag, 0, 1, 1, 1, 0.1, 0.05);
+		addComponent(sendAndShowGrid, eastLayout, sendButton, 0, 2, 1, 1, 0.1, 0.3);
+		/*
+		 * // Set right gridlayout Panel sendAndShowGrid = new Panel(new GridLayout(3,
+		 * 1)); sendAndShowGrid.add(currentOrderList); sendAndShowGrid.add(priceTag);
+		 * sendAndShowGrid.add(sendButton);
+		 */
 
 		// Set Main GridbagLayout
-		addComponent(c, gbl, panelButtons, 0, 0, 1, 1, 1.0, 1.0);
-		addComponent(c, gbl, sendAndShowGrid, 1, 0, 1, 1, 1.0, 1.0);
+		addComponent(mainContainer, gbl, panelButtons, 0, 0, 1, 1, 0.7, 1.0);
+		addComponent(mainContainer, gbl, sendAndShowGrid, 1, 0, 1, 1, 0.3, 1.0);
 
 		// make Fullscreen:
 		Toolkit tk = Toolkit.getDefaultToolkit();
@@ -128,8 +155,8 @@ public class MainGui extends JFrame {
 		int ySize = ((int) tk.getScreenSize().getHeight());
 
 		// show:
-		f.setSize(xSize, ySize);
-		f.setVisible(true);
+		mainJFrame.setSize(xSize, ySize);
+		mainJFrame.setVisible(true);
 
 	}
 
@@ -144,7 +171,6 @@ public class MainGui extends JFrame {
 		return null;
 	}
 
-	// Hilfsklasse für GridbadLayout
 	static void addComponent(Container cont, GridBagLayout gbl, Component c, int x, int y, int width, int height,
 			double weightx, double weighty) {
 		GridBagConstraints gbc = new GridBagConstraints();
@@ -157,6 +183,11 @@ public class MainGui extends JFrame {
 		gbc.weighty = weighty;
 		gbl.setConstraints(c, gbc);
 		cont.add(c);
+	}
+
+	public void resetEast() {
+		currentOrderList.setText("");
+		currentOrderList.setBackground(Color.white);
 	}
 
 }
